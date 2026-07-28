8d7976f00f0a420342033e55cf38765f48c26c2d
Ilya Bogdanov <zeerayne1337@gmail.com>
2026-07-28 07:16:08 +0300

    Migrate `hyprlang` syntax to `lua` for hyprland >= 0.55 (#10)
    * Migrate `hyprlang` syntax to `lua` for hyprland >= 0.55
    
    * Add fixups suggested by @coderabbitai

---
+++ src/hyprprefs.lua
@@ -0,0 +1,9 @@
+-- Manually create this file in the same directory as hyprland.lua
+-- In this file you can add your custom configurations
+-- Example: keyboard layouts
+
+hl.config({
+    input = {
+        kb_layout = "us,de,es"
+    }
+})
